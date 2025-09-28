283d7265067d624cac0c08515f43c618962d5c1a
Vita Khan <vita.khan.2002@mail.ru>
2025-09-28 03:31:08 -0400

    added lab3

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -1,7 +1,81 @@
+import Highlight from "./Highlight";
+import Add from "./Add";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import ArrowFunctions from "./ArrowFunctions";
+import BooleanVariables from "./BooleanVariables";
+import Classes from "./Classes";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import Destructing from "./Destructing";
+import DestructingImports from "./DestructingImports";
+import FilterFunction from "./FilterFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import ForLoops from "./ForLoops";
+import FunctionDestructing from "./FunctionDestructing";
+import House from "./House";
+import IfElse from "./IfElse";
+import ImpliedReturn from "./ImpliedReturn";
+import JsonStringify from "./JsonStringify";
+import LegacyFunctions from "./LegacyFunctions";
+import MapFunction from "./MapFunction";
+import SimpleArrays from "./SimpleArrays";
+import Spreading from "./Spreading";
+import Square from "./Square";
+import Styles from "./Styles";
+import TemplateLiterals from "./TemplateLiterals";
+import TernaryOperator from "./TernaryOperator";
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import PathParameters from "./PathParameters";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
+
 export default function Lab3() {
+    console.log('Hello World!');
     return (
-        <div>
-            <h2>Lab 3</h2>
-        </div>
+    <div id="wd-lab3" className="container">
+      <h3>Lab 3: JavaScript</h3>
+      <VariablesAndConstants/>
+      <VariableTypes/>
+      <BooleanVariables/>
+      <IfElse/>
+      <TernaryOperator/>
+      <ConditionalOutputIfElse/>
+      <ConditionalOutputInline/>
+      <LegacyFunctions/>
+      <ArrowFunctions/>
+      <ImpliedReturn/>
+      <TemplateLiterals/>
+      <SimpleArrays/>
+      <ArrayIndexAndLength/>
+      <AddingAndRemovingToFromArrays/>
+      <ForLoops/>
+      <MapFunction/>
+      <FindFunction/>
+      <FindIndex/>
+      <FilterFunction/>
+      <JsonStringify/>
+      <House/>
+      <Spreading/>
+      <Destructing/>
+      <FunctionDestructing/>
+      <DestructingImports/>
+      <Classes/>
+      <Styles/>
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+     </Highlight>
+     <PathParameters/>
+     <TodoItem/>
+     <TodoList/>
+    </div>
+
     );
 }
--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -1,22 +1,22 @@
 'use client';
- 
-import Nav from "react-bootstrap/Nav";
+import { Nav } from "react-bootstrap";
 import Link from "next/link";
- 
+import { usePathname } from "next/navigation";
 export default function TOC() {
+ const pathname = usePathname();
   return (
     <Nav variant="pills">
       <Nav.Item>
-        <Nav.Link as={Link} href="/Labs">Labs</Nav.Link>
+        <Nav.Link as={Link} href="/Labs" className={`nav-link ${pathname.endsWith("Labs") ? "active" : ""}`}>Labs</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link as={Link} href="/Labs/Lab1">Lab 1</Nav.Link>
+        <Nav.Link as={Link} href="/Labs/Lab1" className={`nav-link ${pathname.endsWith("Lab1") ? "active" : ""}`}>Lab 1</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link as={Link} href="/Labs/Lab2">Lab 2</Nav.Link>
+        <Nav.Link as={Link} href="/Labs/Lab2" className={`nav-link ${pathname.endsWith("Lab2") ? "active" : ""}`}>Lab 2</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link as={Link} href="/Labs/Lab3">Lab 3</Nav.Link>
+        <Nav.Link as={Link} href="/Labs/Lab3" className={`nav-link ${pathname.endsWith("Lab3") ? "active" : ""}`}>Lab 3</Nav.Link>
       </Nav.Item>
       <Nav.Item>
         <Nav.Link as={Link} href="/">Kambaz</Nav.Link>
