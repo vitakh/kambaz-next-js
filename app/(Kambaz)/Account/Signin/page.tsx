6c67860db235a87d8b99d4d31621ac0e73d263d4
Vita Khan <vita.khan.2002@mail.ru>
2025-11-09 14:50:34 -0500

    error

--- app/(Kambaz)/Account/Signin/page.tsx
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -5,35 +5,45 @@ import { setCurrentUser } from "../reducer";
 import { useDispatch } from "react-redux";
 import Link from "next/link";
 import { Button, Form } from "react-bootstrap";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation"; // ← replace redirect with this
 import * as client from "../client";
-
+ 
 export default function Signin() {
-       const [credentials, setCredentials] = useState<any>({});
+  const [credentials, setCredentials] = useState<any>({});
   const dispatch = useDispatch();
+  const router = useRouter();
+ 
   const signin = async () => {
     const user = await client.signin(credentials);
     if (!user) return;
     dispatch(setCurrentUser(user));
-    redirect("/Dashboard");
+    console.log("Sign in");
+    router.push("/Dashboard");
   };
-
- return (
-   <div id="wd-signin-screen" className="ms-3">
-      <h1>Sign in</h1>
-      <Form.Control id="wd-username"
-             placeholder="username"
-             className="mb-2 w-50" 
-             defaultValue={credentials.username}
-             onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}/>
-      <Form.Control id="wd-password"
-             placeholder="password" type="password"
-             className="mb-2 w-50"
-             defaultValue={credentials.password}
-             onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}/>
-      <Button id="wd-signin-btn" variant="primary"
-            className="w-50 mb-2" onClick={signin}>
-            Sign in </Button><br/>
-      <Link id="wd-signup-link" href="/Account/Signup">Sign up</Link>
-    </div> );
-    }
+ 
+  return (
+<div id="wd-signin-screen" className="ms-3">
+<h1>Sign in</h1>
+<Form.Control
+        id="wd-username"
+        placeholder="username"
+        className="mb-2 w-50"
+        defaultValue={credentials.username}
+        onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+      />
+<Form.Control
+        id="wd-password"
+        placeholder="password"
+        type="password"
+        className="mb-2 w-50"
+        defaultValue={credentials.password}
+        onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+      />
+<Button id="wd-signin-btn" variant="primary" className="w-50 mb-2" onClick={signin}>
+        Sign in
+</Button>
+<br />
+<Link id="wd-signup-link" href="/Account/Signup">Sign up</Link>
+</div>
+  );
+}
